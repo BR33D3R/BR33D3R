0f2d2d078f98c5e715242413fdbf5f195b357b55
BR33D3R <141458032+BR33D3R@users.noreply.github.com>
2023-08-06 02:37:14 -0700

    Update deploy.ts

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -1,24 +1,50 @@
-import { ethers } from "hardhat";
+const hre = require("hardhat");
 
 async function main() {
-  const currentTimestampInSeconds = Math.round(Date.now() / 1000);
-  const unlockTime = currentTimestampInSeconds + 60;
+  const [deployer] = await hre.ethers.getSigners();
 
-  const lockedAmount = ethers.utils.parseEther("0.001");
+  console.log("Deploying contracts with the account:", deployer.address);
 
-  const Lock = await ethers.getContractFactory("Lock");
-  const lock = await Lock.deploy(unlockTime, { value: lockedAmount });
+  // Deploy Sprout
+  const Sprout = await hre.ethers.getContractFactory("Sprout");
+  const sprout = await Sprout.deploy(/* constructor arguments here */);
+  await sprout.deployed();
+  console.log("Sprout deployed to:", sprout.address);
 
-  await lock.deployed();
+  // Deploy S33DFactory
+  const S33DFactory = await hre.ethers.getContractFactory("S33DFactory");
+  const s33dFactory = await S33DFactory.deploy(/* constructor arguments here */);
+  await s33dFactory.deployed();
+  console.log("S33DFactory deployed to:", s33dFactory.address);
 
-  console.log(
-    `Lock with ${ethers.utils.formatEther(lockedAmount)}ETH and unlock timestamp ${unlockTime} deployed to ${lock.address}`
-  );
+  // Deploy D1RT
+  const D1RT = await hre.ethers.getContractFactory("D1RT");
+  const d1rt = await D1RT.deploy(/* constructor arguments here */);
+  await d1rt.deployed();
+  console.log("D1RT deployed to:", d1rt.address);
+
+  // Deploy S33D3xCH4NG3
+  const S33D3xCH4NG3 = await hre.ethers.getContractFactory("S33D3xCH4NG3");
+  const s33d3xch4ng3 = await S33D3xCH4NG3.deploy(/* constructor arguments here */);
+  await s33d3xch4ng3.deployed();
+  console.log("S33D3xCH4NG3 deployed to:", s33d3xch4ng3.address);
+
+  // Deploy S33D3xCH4NG3Factory
+  const S33D3xCH4NG3Factory = await hre.ethers.getContractFactory("S33D3xCH4NG3Factory");
+  const s33d3xch4ng3Factory = await S33D3xCH4NG3Factory.deploy(/* constructor arguments here */);
+  await s33d3xch4ng3Factory.deployed();
+  console.log("S33D3xCH4NG3Factory deployed to:", s33d3xch4ng3Factory.address);
+
+  // Deploy S33DSW4P
+  const S33DSW4P = await hre.ethers.getContractFactory("S33DSW4P");
+  const s33dsw4p = await S33DSW4P.deploy(/* constructor arguments here */);
+  await s33dsw4p.deployed();
+  console.log("S33DSW4P deployed to:", s33dsw4p.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
-main().catch((error) => {
-  console.error(error);
-  process.exitCode = 1;
-});
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error);
+    process.exit(1);
+  });
