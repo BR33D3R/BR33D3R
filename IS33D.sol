4bccab173009991f58379891a482b29bbd80fc3a
BR33D3R <141458032+BR33D3R@users.noreply.github.com>
2023-08-05 19:57:08 -0700

    Delete IS33D.sol
    redundant

--- IS33D.sol
+++
@@ -1,6 +0,0 @@
-interface IS33D {
-    function initialize(string memory _genus, string memory _species, string memory _variety) external;
-    function genus() external view returns (string memory);
-    function species() external view returns (string memory);
-    function variety() external view returns (string memory);
-}
