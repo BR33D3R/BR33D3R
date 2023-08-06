09d994db9e75520388122e89ef1b20e306405fe5
BR33D3R <141458032+BR33D3R@users.noreply.github.com>
2023-08-06 01:39:09 -0700

    Update D1RT.sol

--- D1RT.sol
+++ D1RT.sol
@@ -1,124 +1,84 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+// Import necessary contracts and utilities from OpenZeppelin and local contracts.
+import "./S33DSFactory.sol";
+import "./Sprout.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
 import "@openzeppelin/contracts/utils/Create2.sol";
-import "@openzeppelin/contracts/utils/Counters.sol"; // Import the Counters library
-import "@openzeppelin/contracts/token/ERC721/IERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol";
 
-/// @title Interface for the Sprout contract.
-/// @dev This interface is used to initialize the sprout contract.
-interface ISprout {
-    function initialize(address owner, string memory _genus, string memory _species, string memory _variety) external;
-}
+/**
+ * @title D1RT
+ * @dev A contract that inherits from ERC721 and implements IERC721Receiver. It allows the user to plant S33Ds and create Sprouts.
+ */
+contract D1RT is ERC721, IERC721Receiver {
 
-/// @title Interface for S33DS contract.
-/// @dev It is an ERC721 interface with an additional burn function.
-interface IS33DS is IERC721 {
-    function burn(uint256 tokenId) external;
-}
-
-/// @title Sprout Contract
-/// @dev This contract is an ERC721 token that represents a sprout.
-contract Sprout is ERC721, Ownable, ISprout {
-    string public genus;
-    string public species;
-    string public variety;
-
-    constructor() ERC721("Sprout", "SPT") {}
-
-    /// @notice Initializes the sprout with its owner and its properties.
-    /// @param owner The address of the owner of the sprout.
-    /// @param _genus The genus of the sprout.
-    /// @param _species The species of the sprout.
-    /// @param _variety The variety of the sprout.
-    function initialize(address owner, string memory _genus, string memory _species, string memory _variety) external override {
-        transferOwnership(owner);
-        genus = _genus;
-        species = _species;
-        variety = _variety;
-    }
-}
-
-/// @title D1RT Contract
-/// @dev This contract is used to plant S33D tokens and create sprout tokens.
-contract D1RT is Ownable {
+    // Using OpenZeppelin's Counters utility for keeping track of token IDs.
     using Counters for Counters.Counter;
     Counters.Counter private _tokenIdCounter;
 
-    bytes32 public constant SPROUT_BYTECODE_HASH = keccak256(type(Sprout).creationCode);
+    // Instance of S33DSFactory.
+    S33DSFactory private _s33ds;
 
+    // Mapping to keep track of Sprouts associated with a particular token ID.
     mapping(uint256 => address) public getSprout;
 
-    IS33DS private _s33ds;
+    // Event to notify when a new Sprout is planted.
+    event SproutPlanted(address indexed sproutAddress, uint256 indexed sproutId);
 
-    /// @notice Initializes the contract with a S33DS contract.
-    /// @param s33ds The S33DS contract.
-    constructor(IS33DS s33ds) {
-    _s33ds = s33ds;
+    /**
+     * @dev Constructor that sets the name and symbol of the ERC721 contract and the S33DSFactory instance.
+     */
+    constructor(address s33dsAddress) ERC721("D1RT", "D1RT") {
+        _s33ds = S33DSFactory(s33dsAddress);
     }
 
-    /// @notice Plants a S33D token and creates a new sprout token.
-    /// @dev This function will burn the S33D token and deploy a new Sprout contract with the same properties.
-    /// @param tokenId The ID of the S33D token to plant.
-    function plantS33D(uint256 tokenId) public {
+    /**
+     * @dev Function to plant a S33D, create a new Sprout, and increment the token ID counter.
+     */
+    function plantS33D(uint256 tokenId, uint256 _plantType) public {
+        require(_s33ds.getApproved(tokenId) == address(this), "D1RT is not approved to transfer this S33D");
+
+        // Transfer the S33D token to this contract.
         _s33ds.safeTransferFrom(msg.sender, address(this), tokenId);
-        address s33dAddress = getS33D[tokenId];
-        IS33D s33d = IS33D(s33dAddress);
+
+        // Get the S33D token's details.
+        address s33dAddress = _s33ds.getS33D(tokenId);
+        S33D s33d = S33D(s33dAddress);
         string memory _genus = s33d.genus();
         string memory _species = s33d.species();
         string memory _variety = s33d.variety();
+        uint256 _flowerCount = 1; // You need to figure out how to handle this count.
 
+        // Increment the token ID counter and get the new token ID.
         _tokenIdCounter.increment();
         uint256 newSproutId = _tokenIdCounter.current();
         bytes32 saltValue = bytes32(newSproutId);
 
+        // Create a new Sprout contract using Create2 for deterministic deployment.
         address sproutAddress = Create2.deploy(0, saltValue, type(Sprout).creationCode);
-
         require(sproutAddress != address(0), "Sprout deployment failed");
 
+        // Map the new token ID to the Sprout's address.
         getSprout[newSproutId] = sproutAddress;
 
-        ISprout(sproutAddress).initialize(msg.sender, _genus, _species, _variety, _type, _flowerCount);
+        // Initialize the new Sprout contract with the S33D's details.
+        Sprout sprout = Sprout(sproutAddress);
+        sprout.initialize(msg.sender, _plantType, _flowerCount, _genus, _species, _variety, _s33ds);
 
+        // Burn the S33D token.
         _s33ds.burn(tokenId);
 
+        // Emit the SproutPlanted event.
         emit SproutPlanted(sproutAddress, newSproutId);
     }
 
-    /// @notice Confirms the creation of a sprout.
-    /// @dev This function will burn the S33D token and deploy a new Sprout contract with the same properties.
-    /// @param tokenId The ID of the S33D token to confirm the sprout.
-    /// @param _genus The genus of the sprout.
-    /// @param _species The species of the sprout.
-    /// @param _variety The variety of the sprout.
-    function confirmSprout(uint256 tokenId, string memory _genus, string memory _species, string memory _variety) public onlyOwner {
-        _tokenIdCounter.increment();
-
-        uint256 newSproutId = _tokenIdCounter.current();
-        bytes32 saltValue = bytes32(newSproutId);
-
-        address sproutAddress = Create2.deploy(0, saltValue, type(Sprout).creationCode);
-
-        require(sproutAddress != address(0), "Sprout deployment failed");
-
-        getSprout[newSproutId] = sproutAddress;
-
-        ISprout(sproutAddress).initialize(msg.sender, _genus, _species, _variety);
-
-        _s33ds.burn(tokenId);
-
-        emit SproutConfirmed(sproutAddress, newSproutId);
+    /**
+     * @dev Implementation of IERC721Receiver's onERC721Received function. Allows this contract to receive ERC721 tokens.
+     */
+    function onERC721Received(address, address, uint256, bytes memory) public virtual override returns (bytes4) {
+        return this.onERC721Received.selector;
     }
-    
-    /// @notice Emits when a sprout is planted.
-    /// @param sproutAddress The address of the newly deployed sprout contract.
-    /// @param tokenId The ID of the new sprout token.
-    event SproutPlanted(address indexed sproutAddress, uint256 indexed tokenId);
-
-    /// @notice Emits when a sprout is confirmed.
-    /// @param sproutAddress The address of the confirmed sprout contract.
-    /// @param tokenId The ID of the confirmed sprout token.
-    event SproutConfirmed(address indexed sproutAddress, uint256 indexed tokenId);
 }
