88da846102c2c0fa3c2db6377c56a4503bed4140
BR33D3R <141458032+BR33D3R@users.noreply.github.com>
2023-08-07 13:57:30 -0700

    Update Sprout.sol
    updated logic for harvest function and cleaned up code by putting interfaces in seperate files in a folder

--- contracts/S01L/Sprout.sol
+++ contracts/S01L/Sprout.sol
@@ -5,25 +5,15 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/proxy/Clones.sol";
-import "S33DSFactory.sol";
+import "@openzeppelin/contracts/utils/Counters.sol";
+import "./interfaces/ISprout.sol";
+import "./interfaces/ID1RT.sol";
+import "./S01L.sol";
+import "./S33D2.sol";
 
-interface ISprout {
-    function initialize(
-        address owner,
-        uint256 _type,
-        uint256 _flowerCount,
-        string memory _genus,
-        string memory _species,
-        string memory _variety
-    ) external;
-    function cloneSprout(address newOwner) external returns (address);
-}
-
-interface ID1RT {
-    function sproutImplementation() external view returns (address);
-    function updateSproutImplementation(address newImplementation) external;
-}
-
+/// @title Sprout Contract
+/// @notice This contract represents a unique plant that can be cloned, pollinated and harvested. 
+/// @dev Contract is ownable, ensuring certain actions can only be performed by the plant owner.
 contract Sprout is ERC721, Ownable {
     uint256 public plantType; // 0 = flower, 1 = pollen
     string public genus;
@@ -41,12 +31,30 @@ contract Sprout is ERC721, Ownable {
     bool public isHarvested = false;
 
     address public parent;
-    IS33DSFactory private _s33ds;
+    S01L private _s01l;
+    S33D2 private _s33d2;
 
-    constructor(IS33DSFactory s33ds) ERC721("Sprout", "SPT") {
-        _s33ds = s33ds;
-    }
+    // Track the current generation
+    uint256 public currentGeneration = 0;
 
+    // Mapping of generations to array of seeds
+    mapping(uint256 => string[]) public generationSeeds;
+
+    // Mapping of seeds to their generation
+    mapping(string => uint256) public seedGenerations;
+
+    /// @notice Constructs an instance of the Sprout contract.
+    /// @dev The initial state of the plant is not flowering, not pollinated, and not harvested.
+    constructor() ERC721("Sprout", "SPT") {}
+
+    /// @notice Initializes the plant with given parameters.
+    /// @dev The function can only be called by the contract owner.
+    /// @param owner The owner of the plant.
+    /// @param _plantType The type of the plant (0 for flower, 1 for pollen).
+    /// @param _flowerCount The number of flowers the plant will produce when it's harvested.
+    /// @param _genus The genus of the plant.
+    /// @param _species The species of the plant.
+    /// @param _variety The variety of the plant.
     function initialize(
         address owner,
         uint256 _plantType,
@@ -63,26 +71,40 @@ contract Sprout is ERC721, Ownable {
         variety = _variety;
         cloningPeriodStart = block.timestamp + 2 weeks;
     }
+
+    /// @notice Sets the flowering state of the plant.
+    /// @dev The function can only be called by the plant owner.
+    /// @param _isFlowering The new flowering state of the plant.
     function setIsFlowering(bool _isFlowering) external onlyOwner {
         isFlowering = _isFlowering;
     }
 
+    /// @notice Sets the pollinated state of the plant.
+    /// @dev The function can only be called by the plant owner.
+    /// @param _isPollinated The new pollinated state of the plant.
     function setIsPollinated(bool _isPollinated) external onlyOwner {
         isPollinated = _isPollinated;
     }
 
+    /// @notice Sets the harvested state of the plant.
+    /// @dev The function can only be called by the plant owner.
+    /// @param _isHarvested The new harvested state of the plant.
     function setIsHarvested(bool _isHarvested) external onlyOwner {
         isHarvested = _isHarvested;
     }
 
+    /// @notice Starts the flowering process of the plant.
+    /// @dev The function can only be called by the plant owner. The plant cannot already be flowering.
+    /// @param _pollinationPeriod The length of the pollination period in seconds.
     function startFlowering(uint256 _pollinationPeriod) external onlyOwner {
         require(!isFlowering, "Already flowering");
         isFlowering = true;
         pollinationPeriod = block.timestamp + _pollinationPeriod;
     }
 
-
-
+    /// @notice Pollinates the plant.
+    /// @dev The function can only be called by the plant owner. The plant must be flowering and not already pollinated, and it must be within the pollination period.
+    /// @param pollen The address of the pollen to use.
     function pollinate(address pollen) external onlyOwner {
         require(isFlowering && !isPollinated && block.timestamp <= pollinationPeriod, "Cannot pollinate at this time");
         
@@ -93,6 +115,11 @@ contract Sprout is ERC721, Ownable {
         pollinationCount++;
     }
 
+    /// @notice Clones the plant and transfers ownership to a new owner.
+    /// @dev The function can only be called by the plant owner. The plant cannot be flowering, and it must be after the cloning period start time.
+    /// @param newOwner The owner of the new cloned plant.
+    /// @param d1rtAddress The address of the D1RT contract.
+    /// @return The address of the new cloned plant.
     function cloneSprout(address newOwner, address d1rtAddress) external onlyOwner returns (address) {
         require(!isFlowering && block.timestamp >= cloningPeriodStart, "Cannot clone at this time");
 
@@ -106,14 +133,18 @@ contract Sprout is ERC721, Ownable {
 
         return sproutAddress;
     }
-    
+
+    /// @notice Harvests the plant by minting seed tokens.
+    /// @dev The function can only be called by the plant owner. The plant must be pollinated and not already harvested.
     function harvest() external onlyOwner {
-        require(isPollinated && !isHarvested && block.timestamp > pollinationPeriod, "Cannot harvest at this time");
+        require(isPollinated && !isHarvested, "Cannot harvest at this time");
         isHarvested = true;
 
         for (uint256 i = 0; i < flowerCount; i++) {
-            // Mint a new S33D for each flower.
-           _s33ds.createS33D(genus, species, variety);
+            _s33d2.mintForOwner(owner(), genus, species, variety);
         }
-        }
+
+        // increment the currentGeneration
+        currentGeneration++;
     }
+}
