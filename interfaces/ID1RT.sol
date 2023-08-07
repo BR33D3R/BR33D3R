dc0728c3fbad8314d3a903753cb9f689fc2b1af4
BR33D3R <141458032+BR33D3R@users.noreply.github.com>
2023-08-07 14:01:00 -0700

    Delete interfaces directory

--- interfaces/ID1RT.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.20;
-
-
-interface ID1RT {
-    function sproutImplementation() external view returns (address);
-    function updateSproutImplementation(address newImplementation) external;
-}
