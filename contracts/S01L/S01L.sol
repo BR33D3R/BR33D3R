6912130ecdca7d7064efafb13623c56578584c3e
BR33D3R <141458032+BR33D3R@users.noreply.github.com>
2023-08-09 15:19:29 -0700

    Update S01L.sol

--- contracts/S01L/S01L.sol
+++ contracts/S01L/S01L.sol
@@ -5,9 +5,10 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "./S33DFactory.sol";
 import "./CloneFactory.sol";
-import "./libraries/S01LLibrary.sol";
-import "./interfaces/IS33D.sol";
+import "../Libraries/S01LLibrary.sol";
+import "../interfaces/IS33D.sol";
 import "./Sprout.sol";
+
 
 /// @title S01L contract
 /// @notice This contract handles interactions with S33D and Sprout contracts.
@@ -18,23 +19,16 @@ contract S01L is Ownable, CloneFactory, S33DFactory {
     /// @dev Storage for the contract data
     S01LLibrary.Data private s01lData;
 
-    /// @notice Registers a new S33D contract.
-    /// @dev This function can only be called by the owner.
-    /// @param s33dContract The address of the S33D contract to register.
-    /// @return The address of the registered S33D contract.
-    function S0WS33D(address s33dContract) public onlyOwner returns (address) {
-        s01lData.addS33DContract(s33dContract);
-        return s33dContract;
+    // Mapping to store the addresses of S33D contracts
+    mapping(uint256 => address) public s33dContracts;
+
+    // Mapping to store the addresses of Sprout contracts
+    mapping(uint256 => address) public sproutContracts;
+
+    constructor() S33DFactory(S01L(address(this))) {
+ 
     }
 
-    /// @notice Registers a new S33D2 contract.
-    /// @dev This function can only be called by the owner.
-    /// @param s33dContract The address of the S33D2 contract to register.
-    /// @return The address of the registered S33D2 contract.
-    function S0WS33D2(address s33dContract) public onlyOwner returns (address) {
-        s01lData.addS33DContract(s33dContract);
-        return s33dContract;
-    }
 
     /// @notice Plants a new Sprout contract.
     /// @dev This function can only be called by the owner.
@@ -44,19 +38,46 @@ contract S01L is Ownable, CloneFactory, S33DFactory {
         return s01lData.plantSprout(address(newSprout));
     }
 
+    function cutClone(
+        address newOwner, 
+        uint256 plantType, 
+        uint256 flowerCount, 
+        string memory genus, 
+        string memory species, 
+        string memory variety
+    ) external virtual override returns (address) {
+        require(msg.sender == address(sproutImplementation), "Only original Sprout can clone");
+
+        address clonedSprout = Clones.clone(sproutImplementation);
+        ISprout(clonedSprout).initialize(newOwner, plantType, flowerCount, genus, species, variety);
+        
+        return clonedSprout;
+    }
+
     /// @notice Burns a token and plants a new Sprout contract.
     /// @dev This function requires the caller to be whitelisted and to own the token.
     /// @param tokenId The ID of the token to burn.
     function burnAndPlant(uint256 tokenId) public {
         require(s01lData.isWhitelisted(msg.sender), "Only whitelisted contracts can burn and plant");
 
-        if (msg.sender == s01lData.getS33DContract(s01lData.getContractIdCounter()) || msg.sender == s01lData.getS33DContract(s01lData.getSproutIdCounter())) {
-            IS33D s33dContract = IS33D(msg.sender);
-            require(s33dContract.ownerOf(tokenId) == msg.sender, "Only owner can burn and plant");
-            s33dContract.burn(tokenId); // Burn the NFT
+        // Get the relevant contract address
+        address relevantS33DContract;
+        if (msg.sender == s01lData.getS33DContract(s01lData.getContractIdCounter())) {
+            relevantS33DContract = s01lData.getS33DContract(s01lData.getContractIdCounter());
+        } else if (msg.sender == s01lData.getS33DContract(s01lData.getSproutIdCounter())) {
+            relevantS33DContract = s01lData.getS33DContract(s01lData.getSproutIdCounter());
+        } else {
+            revert("Invalid sender");
         }
 
-        plantSprout(); // Create a new sprout
+        IS33D s33dContract = IS33D(relevantS33DContract);
+        require(s33dContract.ownerOf(tokenId) == msg.sender, "Only owner can burn and plant");
+
+        // Burn the NFT
+        s33dContract.burn(tokenId);
+
+        // Create a new sprout
+        plantSprout();
     }
 
     /// @notice Retrieves the address of a S33D contract by ID.
